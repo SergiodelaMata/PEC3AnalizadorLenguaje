5792d438ab0bbf047a7f2d839b8bb2958d76e5f1
nurihp <nuria.higuera@edu.uah.es>
2019-12-15 22:22:50 +0100

    Añadido grafo

--- antlr/gramaticas/TablaSimbolosComplejidad.java
+++ antlr/gramaticas/TablaSimbolosComplejidad.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 
 public class TablaSimbolosComplejidad
@@ -32,14 +33,52 @@ public class TablaSimbolosComplejidad
         return tablaSimbolos;
     }
 
-    public void printHashMapFunction(int position) //Imprime el recorrido del grafo en una función
+    public void crearGrafo(int posicion)
     {
         TablaSimbolosComplejidadFuncion tablaSimbolos = new TablaSimbolosComplejidadFuncion();
-        //System.out.println("Hola soy Elmo");
+        String nombreFuncion = tablaSimbolos.getFunctionName();
+        nombreFuncion = nombreFuncion.substring(0, nombreFuncion.indexOf("(") + 1);
+        String dot = printHashMapFunction(posicion);
+        try
+        {
+            //String nombreDot = "grafo" + nombreFuncion + ".dot";
+            FileWriter filewriter = new FileWriter("grafo" + nombreFuncion + ".dot"); //archivo donde se va a crear
+            PrintWriter printw = new PrintWriter(filewriter);     
+            //String svg = "dot -Tsvg " + nombreDot + "-o grafo" + nombreFuncion +".svg";   
+            //Runtime.getRuntime().exec(svg);
+            Runtime.getRuntime().exec("dot -Tsvg grafo"+ nombreFuncion + ".dot -o grafo" + nombreFuncion +".svg");
+            printw.println(dot);
+            printw.close();
+        } catch(IOException io)
+        {
+            System.out.println("Error al crear grafo");
+        }
+    }
+
+    public String printHashMapFunction(int position) //Imprime el recorrido del grafo en una función
+    {
+        String dot = "digraph Grafo\n{\n";
+        TablaSimbolosComplejidadFuncion tablaSimbolos = new TablaSimbolosComplejidadFuncion();
         tablaSimbolos = getTable(position);
         System.out.println(tablaSimbolos.getFunctionName());
         Enumeration enumeration = symbolTable.keys();
         ArrayList<Integer> listNumNodes = new ArrayList<Integer>();
+        ArrayList<Integer> listaNodos = tablaSimbolos.getListNode();
+        for (int i=0; i<listaNodos.size(); i++)
+        {
+            if (i == 0)
+            {
+               dot += "0 [label=\"\"style=filled, fillcolor=red]\n";
+            }
+            else if (i == listaNodos.size()-1)
+            {
+                dot += (listaNodos.size()-1) + " [label=\"\"style=filled, fillcolor=blue]\n";
+            }
+            else
+            {
+                dot += i + " [label=\"\"];\n";
+            }
+        }
         for(int i = 0; i < tablaSimbolos.size(); i++)
         {
             listNumNodes = tablaSimbolos.get(i);
@@ -49,6 +88,7 @@ public class TablaSimbolosComplejidad
               for(int j = 0; j < listNumNodes.size(); j++)
               {
                   System.out.println(i + " -> " + listNumNodes.get(j));
+                  dot += i + "->" + listNumNodes.get(j) + ";\n";
               }
             }
             else
@@ -56,7 +96,9 @@ public class TablaSimbolosComplejidad
               System.out.print(i + " no tiene hijos.");
             }
         }
-
+        dot += "}";
+        System.out.println(dot);
+        return dot;
     }
 
     public void printHashMap()
@@ -65,6 +107,7 @@ public class TablaSimbolosComplejidad
         for(int i = 0; i < symbolTable.size(); i++)
         {
             printHashMapFunction(i);
+            crearGrafo(i);
         }
     }
 
